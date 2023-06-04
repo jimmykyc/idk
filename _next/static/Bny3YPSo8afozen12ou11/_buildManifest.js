9a626281418d4c16c1671a236092c0f739448347
Jimmy Dev <81708489+jimmykyc@users.noreply.github.com>
2023-06-05 00:49:56 +0800

    Delete _next/static directory

--- _next/static/Bny3YPSo8afozen12ou11/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7ddd580bfd167c22.js"],"/_error":["static/chunks/pages/_error-d419484d69bbcf53.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
