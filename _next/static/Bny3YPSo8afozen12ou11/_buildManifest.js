abaf266a0bebb1fe2d7d79980aa725bbbc514e1c
Jimmy Dev <81708489+jimmykyc@users.noreply.github.com>
2023-06-05 00:25:47 +0800

    Add files via upload

---
+++ _next/static/Bny3YPSo8afozen12ou11/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7ddd580bfd167c22.js"],"/_error":["static/chunks/pages/_error-d419484d69bbcf53.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
